8576c7d90eca32df6bf7bc78b3a829bbce489543
Dauv Miracle <naga7miracle@gmail.com>
2025-08-06 13:04:08 +0700

    Update tst.py
    Signed-off-by: Dauv Miracle <naga7miracle@gmail.com>

--- tst.py
+++ tst.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 
-api_id = 15542958  # Your API ID
-api_hash = "115e7c25b2747bd94757e7d2191f7417"  # Your API Hash
+api_id =   # Your API ID
+api_hash = ""  # Your API Hash
 
 app = Client("aa1", api_id=api_id, api_hash=api_hash)
 
@@ -12,3 +12,4 @@ async def ping(client, message):
 
 if __name__ == "__main__":
     app.run()
+
